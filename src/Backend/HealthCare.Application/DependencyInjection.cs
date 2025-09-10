08ca86878679bb6ceb5b99a2cde7f22bf0543e35
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2025-09-10 16:39:07 +0200

    Added a DependencyInjection container for easier implementation of services.

---
+++ src/Backend/HealthCare.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using HealthCare.Application.Interfaces.RepositoryInterfaces;
+using HealthCare.Application.Interfaces.ServiceInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthCare.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        
+        services.AddScoped<IProductService, IProductService >();
+
+        return services;
+    }
+}
