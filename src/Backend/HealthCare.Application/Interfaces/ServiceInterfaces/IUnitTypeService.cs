f861fd6877e253ccd543331515236d688618377f
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2025-09-11 17:27:00 +0200

    Added UnitTypeService and INterface for it.

---
+++ src/Backend/HealthCare.Application/Interfaces/ServiceInterfaces/IUnitTypeService.cs
@@ -0,0 +1,13 @@
+﻿using HealthCare.Application.DataTransferObjects.Product;
+using HealthCare.Application.DataTransferObjects.UnitType;
+
+namespace HealthCare.Application.Interfaces.ServiceInterfaces;
+
+public interface IUnitTypeService
+{
+    Task<IEnumerable<UnitTypeDto>> GetAllAsync();
+    Task<UnitTypeDto?> GetByIdAsync(Guid id);
+    Task<UnitTypeDto> AddAsync(CreateUnitTypeDto createUnitTypeDto);
+    Task<bool> UpdateAsync(UnitTypeDto updateUnitTypeDto, Guid id);
+    Task<bool> DeleteAsync(Guid id);
+}
---
+++ src/Backend/HealthCare.Application/Services/UnitTYpeService.cs
@@ -0,0 +1,32 @@
+﻿using HealthCare.Application.DataTransferObjects.UnitType;
+using HealthCare.Application.Interfaces.ServiceInterfaces;
+
+namespace HealthCare.Application.Services;
+
+public class UnitTypeService : IUnitTypeService
+{
+    public Task<IEnumerable<UnitTypeDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UnitTypeDto?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UnitTypeDto> AddAsync(CreateUnitTypeDto createUnitTypeDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(UnitTypeDto updateUnitTypeDto, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
