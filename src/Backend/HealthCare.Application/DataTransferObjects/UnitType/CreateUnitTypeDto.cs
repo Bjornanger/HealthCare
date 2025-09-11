e91efba769261c65315440b3ab2470f747e57757
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2025-09-11 16:36:56 +0200

    Added Dto for UnitType

---
+++ src/Backend/HealthCare.Application/DataTransferObjects/UnitType/CreateUnitTypeDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthCare.Application.DataTransferObjects.UnitType;
+
+public class CreateUnitTypeDto
+{
+   [Required]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ src/Backend/HealthCare.Application/DataTransferObjects/UnitType/UnitTypeDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthCare.Application.DataTransferObjects.UnitType;
+
+public class UnitTypeDto
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+}
